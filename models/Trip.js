03b8f0466db567f20b72db43e184e455e2c6316f
Garrnete <garrnete_21@yahoo.com>
2025-10-22 14:25:14 -0400

    integrate CRUD

--- models/Trip.js
+++ models/Trip.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
-const tripSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    location: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-    description: { type: String },
-    imageUrl: { type: String },
-  },
-  { timestamps: true }
-);
+const tripSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  location: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  description: String,
+  imageUrl: String,
+});
 
 export default mongoose.model("Trip", tripSchema);
 
